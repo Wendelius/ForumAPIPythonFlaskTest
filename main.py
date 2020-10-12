7b634f2ff62c4f0faac5413f9e3b3430c0ffbb4e
Wendy G <wendy.gasperazzo@gmail.com>
2020-10-12 22:45:22 +0100

    Implement basic in memory CRUD

--- main.py
+++ main.py
@@ -1,23 +1,62 @@
 # This will make the API "work" without us needing to write hundreds of line of code
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse, abort
 
 # This line is required to work with Flask. 
 app = Flask(__name__)
 api = Api(app)
 
-# Here is a sample API "resource" for testing. Remove it once you write the real code
-# Each resource should be declared as a class (an object that will group functions and variables and do something for us)
-# It inherits from Resource as that gives us access to API methods to keep development easy.
-class HelloWorld(Resource):
-    def get(self): # Define what happens when HelloWorld receives a "GET" request
-        return {"data": "Hello World!"}
+# Define the data format of our message put requests
+message_put_args = reqparse.RequestParser()
+message_put_args.add_argument("message_id", type=int, help="the identifier of the message is required", required=True)
+message_put_args.add_argument("topic", type=str, help="Topic of the message is required", required=True)
+message_put_args.add_argument("body", type=str, help="Body  of the message is required", required=True)
+message_put_args.add_argument("author", type=str, help="Author  of the message")
+message_put_args.add_argument("views", type=str, help="Views  of the message")
 
-    def post(self): # Define what happens when HelloWorld receives a "POST" request
-        return {"data": "Posted"}
+# Sample data testing parameters to GET
+# message {"id": 0, "topic":"?", "body":"?", "author":"?", "likedBy": [], "views": 0}
+messages = {}
+
+# Functions to check for issues with the requests
+
+def abort_if_message_id_not_found(message_id):
+    if message_id not in messages:
+        abort(404, message="Message id is not valid")
+
+def abort_if_message_id_exists(message_id):
+    if message_id in messages:
+        abort(409, message="Message with that id already created")
+
+# Our forum deals with the following resources:
+
+class Messages(Resource):
+    def get(self, message_id): # Define what happens when Messages receives a "GET" request with a message_id parameter
+        abort_if_message_id_not_found(message_id)
+        # messages[message_id]["views"] = messages[message_id]["views"] + 1
+        return messages[message_id]
+
+    def post(self):
+        args = message_put_args.parse_args()
+        id = args["message_id"]
+        abort_if_message_id_exists(id)
+        messages[id] = args
+        return messages[id], 201 # return added data with Created status code
+
+    def put(self, message_id):
+        # Parse the arguments received and ensure they match the expected format
+        args = message_put_args.parse_args()
+        messages[message_id] = args
+        return messages[message_id], 201 # return added data with Created status code
+
+    def delete(self, message_id):
+        abort_if_message_id_not_found(message_id)
+        del messages[message_id]
+        return "", 204
+
 
 # Define the resources that the API knows about and what route (URL) they are found at. Add your own here.
-api.add_resource(HelloWorld, "/helloworld")
+api.add_resource(Messages, "/messages", "/messages/<int:message_id>")
 
 # Used to start the program when calling python main.py on the command line
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -10,12 +10,50 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-# Test HellowWorld GET
-response = requests.get(BASE + "helloworld")
+# Test Invalid Messages GET
+print("GET: ")
+response = requests.get(BASE + "messages/6")
 print(response.status_code)
 print(response.json())
 
-# Test HellowWorld POST
-response = requests.post(BASE + "helloworld")
+# Test Invalid Messages DEL
+print("DEL: ")
+response = requests.delete(BASE + "messages/6")
 print(response.status_code)
 print(response.json())
+
+# Test Messages POST
+test_data = [{"message_id":1, "topic": "Test", "body": "A clever post", "author": "soso", "views":0},
+            {"message_id":2, "topic": "Test2", "body": "A clever post2", "author": "soso", "views":10},
+            {"message_id":3, "topic": "Test3", "body": "A clever post3", "author": "soso", "views":20}]
+
+print("POST: ")
+for i in range(len(test_data)):
+    response = requests.post(BASE + "messages", test_data[i])
+    print(response.json())
+
+# Test Messages PUT
+print("PUT: ")
+response = requests.put(BASE + "messages/4", {"message_id":4, "topic": "Test", "body": "A clever post", "author": "soso", "views":0})
+print(response.json())
+
+# Test Valid Messages GET
+print("GET: ")
+response = requests.get(BASE + "messages/4")
+print(response.json())
+
+# Test Messages PUT
+print("PUT (Update): ")
+response = requests.put(BASE + "messages/4", {"message_id":4, "topic": "New", "body": "A new clever post", "author": "soso", "views":2})
+print(response.json())
+
+# Test Valid Messages GET
+print("GET: ")
+response = requests.get(BASE + "messages/4")
+print(response.json())
+
+# Test Valid Messages DEL
+print("DEL: ")
+response = requests.delete(BASE + "messages/1")
+print(response.status_code)
+
